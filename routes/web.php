1264eaeb1a21e1ca505c7b130553ab7fd1d6651a
Jaytran2205 <Jtran7467@gmail.com>
2026-08-29 20:54:54 +0700

    Add ping diagnostic route

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schema;
 
+Route::get('/ping', fn () => 'pong');
+
 /*
 |--------------------------------------------------------------------------
 | Authentication Routes
